90bbb261e7f77b11ed91684a447b53baaea4d8ca
Vitaliy Sobur <vitaliysobur@fb.com>
2022-09-18 15:36:35 +0200

    ~ token contract minor

--- contracts/CarbonToken.sol
+++ contracts/CarbonToken.sol
@@ -6,10 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CarbonToken is ERC20, Ownable {
-  uint256 public slon = 0;
-
   constructor() ERC20("CarbonToken", "CT") {
-    _mint(msg.sender, 100 * 10 ** 18);
+    _mint(msg.sender, 10000000 * 10 ** 18);
   }
 
   function mint(address to, uint256 amount) public onlyOwner {
